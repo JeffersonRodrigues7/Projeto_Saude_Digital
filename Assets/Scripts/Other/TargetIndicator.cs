739319f15f1ea6b59e942c0809fd8b70dda986cc
Gustavo Alves de Lima <gustavoalves11235@gmail.com>
2023-07-23 23:45:09 -0300

    targetindicator
    inclusao de indicador de objetivo

---
+++ Assets/Scripts/Other/TargetIndicator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    public Transform Target;
+    public float distanciaObjetivo;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Target.position - transform.position;
+
+
+        if(dir.magnitude < distanciaObjetivo)
+        {
+            SetChildrenActive(false);
+        }
+        else
+        {
+            SetChildrenActive(true);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        }
+
+
+    }
+
+    private void SetChildrenActive(bool value)
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+        }
+    }
+}
