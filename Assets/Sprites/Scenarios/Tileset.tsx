845f9da1b7f03f22ca8ca1aec29e301076b6d713
Jefferson Leite Rodrigues Dantas <jefferson_rodrigues7@hotmail.com>
2024-02-18 13:52:40 -0300

    atualizando com sala do novo personagem
    atualizando com sala do novo personagem

---
+++ Assets/Sprites/Scenarios/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="1925" columns="35">
+ <image source="Tileset.png" width="1146" height="1760"/>
+</tileset>
