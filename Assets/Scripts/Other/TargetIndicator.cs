7635bef3ef4a428675feb437063b3f8f7c2f8594
Gustavo Alves de Lima <57842244+GustavoAb10@users.noreply.github.com>
2024-04-16 21:22:19 -0300

    nova maneira de mostrar os objetivos

--- Assets/Scripts/Other/TargetIndicator.cs
+++ Assets/Scripts/Other/TargetIndicator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class TargetIndicator : MonoBehaviour
@@ -11,6 +12,7 @@ public class TargetIndicator : MonoBehaviour
     public Transform toTeacherHouse;
     
     public float distanciaObjetivo;
+    public Text objetivo;
     private Transform Target;
 
     public void ChangeTarget(Transform target)
@@ -20,25 +22,43 @@ public class TargetIndicator : MonoBehaviour
 
     private void validaCenas()
     {
-        if (GameManager.Instance.GetCenaValue("4_3"))
+        if (GameManager.Instance.GetCenaValue("5"))
         {
             ChangeTarget(toSchool);
+            objetivo.text = "Hora de fazer mais uma prova!";
+        }
+        else if (GameManager.Instance.GetCenaValue("4_4"))
+        {
+            ChangeTarget(toAlanHouse);
+            objetivo.text = "Compartilhe com Alan o que aprendeu";
+        }
+        else if (GameManager.Instance.GetCenaValue("4_3"))
+        {
+            ChangeTarget(toSchool);
+            objetivo.text = "Volte à escola para entregar o lanche da professora";
         }
         else if (GameManager.Instance.GetCenaValue("4_minigame"))
         {
             ChangeTarget(toTeacherHouse);
+            objetivo.text = "Devolva a chave para o Rogério";
         }
         else if (GameManager.Instance.GetCenaValue("4_1"))
         {
             ChangeTarget(toTeacherHouse);
+            objetivo.text = "Entenda por qual motivo a professora está sem o seu lanche";
         }
         else if (GameManager.Instance.GetCenaValue("3"))
         {
             ChangeTarget(toSchool);
+            objetivo.text = "Converse com a professora na escola";
         }
         else if (GameManager.Instance.GetCenaValue("1"))
         {
             ChangeTarget(toAlanHouse);
+            objetivo.text = "Entenda porque Alan não foi à escola";
+        }else
+        {
+            objetivo.text = "Algo deu errado, você está sem objetivo";
         }
     }
 
