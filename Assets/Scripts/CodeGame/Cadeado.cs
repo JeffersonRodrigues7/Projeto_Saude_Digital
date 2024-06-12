91f049a324dfad0dba5c77cb52a897d090d932e2
Gustavo Alves de Lima <57842244+GustavoAb10@users.noreply.github.com>
2024-06-11 22:14:41 -0300

    CodeGameMecanica2

--- Assets/Scripts/CodeGame/Cadeado.cs
+++ Assets/Scripts/CodeGame/Cadeado.cs
@@ -9,6 +9,8 @@ public class Cadeado : MonoBehaviour
     public TextMeshProUGUI digito2;
     public TextMeshProUGUI digito3;
 
+    public GameObject arcoCadeado;
+
     public int index1;
     public int index2;
     public int index3;
@@ -35,8 +37,6 @@ public class Cadeado : MonoBehaviour
         }
 
         
-
-
     }
 
     // Update is called once per frame
@@ -45,6 +45,8 @@ public class Cadeado : MonoBehaviour
         if (verificaSenha())
         {
             Debug.Log("Cadeado aberto!");
+            arcoCadeado.transform.localScale = new Vector3(arcoCadeado.transform.localScale.x, 2.0f, arcoCadeado.transform.localScale.z);
+
         }
     }
 
@@ -53,25 +55,71 @@ public class Cadeado : MonoBehaviour
         return digito1.text == "P" && digito2.text == "A" && digito3.text == "Z";
     }
 
+    bool verificaIndexCadeado(int index)
+    {
+        if (index > 26 || index < 0)
+        {
+            return false;
+        }else return true;
+    }
+
+
     public void atualizaLetraAcima(int digito)
     {
         switch (digito)
         {
             case 1:
                 index1 += 1;
-                digito1.text = lista_letras[index1].ToString();
+                if (verificaIndexCadeado(index1))
+                {
+                    digito1.text = lista_letras[index1].ToString();
+                }
                 break;
             case 2:
                 index2 += 1;
-                digito2.text = lista_letras[index2].ToString();
+                if (verificaIndexCadeado(index2))
+                {
+                    digito2.text = lista_letras[index2].ToString();
+                }
                 break;
             case 3:
                 index3 += 1;
-                digito3.text = lista_letras[index3].ToString();
+                if (verificaIndexCadeado(index3))
+                {
+                    digito3.text = lista_letras[index3].ToString();
+                }
                 break;
 
         }
         
     }
+    public void atualizaLetraAbaixo(int digito)
+    {
+        switch (digito)
+        {
+            case 1:
+                index1 -= 1;
+                if (verificaIndexCadeado(index1))
+                {
+                    digito1.text = lista_letras[index1].ToString();
+                }
+                break;
+            case 2:
+                index2 -= 1;
+                if (verificaIndexCadeado(index2))
+                {
+                    digito2.text = lista_letras[index2].ToString();
+                }
+                break;
+            case 3:
+                index3 -= 1;
+                if (verificaIndexCadeado(index3))
+                {
+                    digito3.text = lista_letras[index3].ToString();
+                }
+                break;
 
+        }
+
+    }
 }
